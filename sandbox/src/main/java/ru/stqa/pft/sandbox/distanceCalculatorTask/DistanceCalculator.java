a9c34e146eef85483b5b793c4cc4eda206c8662d
KrisNevermind <marian.kurossu@gmail.com>
2018-06-17 08:40:22 +0200

    formula for distance calc updated

--- sandbox/src/main/java/ru/stqa/pft/sandbox/distanceCalculatorTask/DistanceCalculator.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/distanceCalculatorTask/DistanceCalculator.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox.distanceCalculatorTask;
 public class DistanceCalculator {
 
     public static double distanceCalc(Point p1, Point p2){
-        return Math.sqrt(Math.abs(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y))));
+        return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
 
     }
 }
