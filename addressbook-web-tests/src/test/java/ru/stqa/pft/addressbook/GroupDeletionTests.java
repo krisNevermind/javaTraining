59a234b81fe59e43fc2b49ad2d33a66b0c1166d5
KrisNevermind <marian.kurossu@gmail.com>
2018-06-21 18:22:14 +0200

    method testGroupDeletion renamed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -25,7 +25,7 @@ public class GroupDeletionTests {
     }
     
     @Test
-    public void GroupDeletionTests() {
+    public void testGroupDeletion() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
