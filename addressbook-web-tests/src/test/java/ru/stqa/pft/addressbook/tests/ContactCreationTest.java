3628b8d41248c9eec4fb306a52226abf51d27381
KrisNevermind <marian.kurossu@gmail.com>
2018-06-21 21:03:04 +0200

    Application manager created, tests work

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.NewContactData;
+
+public class ContactCreationTest extends TestBase {
+
+    @Test
+    public void testContactCreation() {
+
+        app.addNewContact();
+        app.fillInContactData(new NewContactData("name1", "name2", "12345", "email@email.com"));
+        app.submitNewContact();
+        app.returnToHomePage();
+    }
+
+}
