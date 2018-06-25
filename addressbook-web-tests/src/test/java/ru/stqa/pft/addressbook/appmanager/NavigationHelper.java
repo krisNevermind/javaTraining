4d533580df7cdf9829049d8e7dc4af0d3bc61f01
KrisNevermind <marian.kurossu@gmail.com>
2018-06-25 08:39:53 +0200

    Browser selection implemented

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,13 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
-    public NavigationHelper(FirefoxDriver wd) {
-
-        super (wd);
+    public NavigationHelper(WebDriver wd) { super (wd);
     }
 
     public void goToGroupPage() {
