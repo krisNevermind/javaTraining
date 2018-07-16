f0b9d5add19187c42eb2c448ec5b63f72fe3fd93
KrisNevermind <marian.kurossu@gmail.com>
2018-07-16 16:28:30 +0200

    GroupDataGen fixed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -23,7 +23,7 @@ public class GroupDataGenerator {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups){
-            writer.write(String.format("&s;&s;&s\n", group.getName(), group.getHeader(), group.getFooter()));
+            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
         }
         writer.close();
     }
