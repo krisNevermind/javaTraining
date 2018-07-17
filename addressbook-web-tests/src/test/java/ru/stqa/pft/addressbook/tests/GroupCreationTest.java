09dbe9acb74790fd3886b0b5f7fc9e3541f26e8c
KrisNevermind <marian.kurossu@gmail.com>
2018-07-17 15:06:49 +0200

    Parametrization Groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,27 +1,40 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withFooter("footer1").withHeader("header1")});
+        list.add(new Object[] {new GroupData().withName("test2").withFooter("footer2").withHeader("header2")});
+        list.add(new Object[] {new GroupData().withName("test3").withFooter("footer3").withHeader("header3")});
+        return list.iterator();
+    }
 
+    @Test (dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("testGroup1");
+  //      GroupData group = new GroupData().withName("testGroup1");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test (enabled = false)
     public void testBadGroupCreation() {
 
         app.goTo().groupPage();
