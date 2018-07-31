88554c4d5f241fa7b4b1dfe29d0291722ada16ba
KrisNevermind <marian.kurossu@gmail.com>
2018-07-31 14:20:27 +0200

    email receiver

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
     private String browser;
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser){
         this.browser = browser;
@@ -75,4 +76,11 @@ public class ApplicationManager {
             }
         return ftp;
         }
+
+    public MailHelper mail(){
+        if (mailHelper == null){
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
+    }
 }
