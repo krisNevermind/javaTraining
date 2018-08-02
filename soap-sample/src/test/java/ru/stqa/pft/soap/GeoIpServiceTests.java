7044738b2593fd3fb5e24444dc649040996112b0
KrisNevermind <marian.kurossu@gmail.com>
2018-08-02 15:02:12 +0200

    iplocationtest

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -1,11 +1,15 @@
 package ru.stqa.pft.soap;
 
+import com.lavasoft.GeoIPService;
+import com.lavasoft.GetCountryISO2ByName;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GeoIpServiceTests {
 
     @Test
     public void testMyIp(){
-        
+        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("193.106.40.57");
+        System.out.println(ipLocation);
     }
 }
